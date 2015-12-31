948905211d5fdc7d9a2c196ec2315b6e2210ee09
yangyong <yyang@ilegendsoft.com>
2015-12-31 15:35:31 +0800

    spring版本修改为3.0.5

--- cloud-code-base/src/main/java/com/maxleap/code/spring/DefaultAutowiredAnnotationBeanPostProcessor.java
+++ cloud-code-base/src/main/java/com/maxleap/code/spring/DefaultAutowiredAnnotationBeanPostProcessor.java
@@ -9,8 +9,9 @@ import org.springframework.beans.factory.support.RootBeanDefinition;
  * Date：15/12/30
  */
 public class DefaultAutowiredAnnotationBeanPostProcessor extends AutowiredAnnotationBeanPostProcessor {
+
   @Override
-  public void postProcessMergedBeanDefinition(RootBeanDefinition beanDefinition,final Class<?> beanType, String beanName) {
+  public void postProcessMergedBeanDefinition(RootBeanDefinition beanDefinition,Class beanType, String beanName) {
     if (MLClassManager.class.isAssignableFrom(beanType)) {//执行MLClassManager类型的自动装载，根据泛型类型来得到对应的beanName
       Class argValue = (Class)beanDefinition.getConstructorArgumentValues().getGenericArgumentValues().get(0).getValue();
       super.postProcessMergedBeanDefinition(beanDefinition,beanType,argValue.getName());
